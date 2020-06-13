a1ac5879724ab3756628af900479ba67cb693ed3
Kucher -mx <max.kucher98@gmail.com>
2020-06-13 22:51:22 +0300

    fixed all issues

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -8,26 +8,24 @@ const rename = require("gulp-rename");
 gulp.task('server', function() {
 
     browserSync({
-        server: {
-            baseDir: "src"
-        }
+        server: {baseDir: '../src'} 
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("../src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("../src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("../src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("../src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
