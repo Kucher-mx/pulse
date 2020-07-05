d32a6ba4fea19ee1af7ab9fd385213a491eb32f1
Kucher -mx <max.kucher98@gmail.com>
2020-07-05 22:14:08 +0300

    added a slider

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+$('.carousel_screen').slick({
+    infinite: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../images/arrow_l.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../images/arrow_r.png" alt=""></button>',
+    autoplay: true,
+    autoplaySpeed: 5000,  
+  });
