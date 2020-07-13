437224538da1753bfd63009b4c629dd1f3e8078a
Kucher -mx <max.kucher98@gmail.com>
2020-07-14 00:10:03 +0300

    smooth pageup

--- src/js/main.js
+++ src/js/main.js
@@ -106,4 +106,21 @@ $(document).ready(function(){
     return false;
   });
 
+  //scroll and pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1000) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+
+  });
+
 });
